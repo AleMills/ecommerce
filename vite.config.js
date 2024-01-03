b4c3b24e9e1ea8abe9aaab72492df335f4842070
alemills <alejandrojofre949@gmail.com>
2024-01-03 17:03:30 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/AleMills.github.io/ecommerce/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/alemills.github.io/ecommerce/' : '/',
 })
 
 
